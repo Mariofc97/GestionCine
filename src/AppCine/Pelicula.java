dae9050a251771361171b56a1bf55eff5a7275a8
Tardes <Tardes@CAS-CLO2-16>
2025-09-11 19:39:55 +0200

    primer commit

---
+++ src/AppCine/Pelicula.java
@@ -0,0 +1,70 @@
+package AppCine;
+
+public class Pelicula {
+
+	private String titulo;
+    private int duracion;
+    private int clasificacionEdad;
+    private double precio;
+    private String id; 
+    
+    
+    public String getTitulo() {
+		return titulo;
+	}
+
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+
+	public int getClasificacionEdad() {
+		return clasificacionEdad;
+	}
+
+
+	public void setClasificacionEdad(int clasificacionEdad) {
+		this.clasificacionEdad = clasificacionEdad;
+	}
+
+
+	public double getPrecio() {
+		return precio;
+	}
+
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public Pelicula(String titulo, int duracion, int clasificacionEdad, double precio, String id) {
+    	
+    this.clasificacionEdad= clasificacionEdad;
+    this.duracion = duracion;
+    this.titulo = titulo;
+    this.precio = precio;
+    this.id = id;
+    }
+}
