66ccd3ca7c9c3805c8128a8922c96abf7b1f88d8
Tardes <Tardes@CAS-CLO2-16>
2025-09-11 19:50:22 +0200

    Usuario clase

---
+++ src/AppCine/Usuario.java
@@ -0,0 +1,47 @@
+package AppCine;
+
+public class Usuario {
+    private String nombre;
+    private String dni;
+    private int edad;
+    public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	private double saldo;
+    
+    public Usuario(String nombre, String dni, int edad, double saldo) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.edad = edad;
+        this.saldo = saldo;
+    }
+}
